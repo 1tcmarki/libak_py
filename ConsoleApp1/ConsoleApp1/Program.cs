f1f01d85229c6417e5fd75e8e61f24f0be4e6204
zwwxxxxx <126754383+1tcmarki@users.noreply.github.com>
2024-03-11 12:07:27 +0100

    Update Program.cs
    elso feladat

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,14 +15,40 @@ namespace ConsoleApp1
                 Console.Write($"{suly}");
             }
         
+
+
+        }
+
+        static int Osszegez(int[] t)
+        {
+            int osszeg=0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i]<=3)
+                {
+                    osszeg = t[i];
+                }
+                
+
+            }
+            return osszeg;
+        }
+        static void kiir_eredmenyek(int roka_libainak_sulya)
+        { 
+        
+        
         }
         static void Main(string[] args)
         {
 
             int[] libak = { 1, 5, 2, 3, 4 };
             Kiir(libak,"libak sulyai");
+            int hany_kilo_libat_ehet_meg_a_roka = Osszegez(libak);
+            
+
             Console.WriteLine("gomb");
             Console.ReadKey();
+            
         }
     }
 }
