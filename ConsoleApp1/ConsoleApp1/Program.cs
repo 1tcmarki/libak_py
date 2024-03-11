669fbaaeb98ddbd380d5cc24136e4e51b152bc97
zwwxxxxx <126754383+1tcmarki@users.noreply.github.com>
2024-03-11 11:49:21 +0100

    libak_kiiratasa
    Libak tomb felvetele es kiiratasa

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Kiir(int[] t,string cim) {
+            Console.WriteLine(cim);
+            foreach (int suly in t)
+            {
+                Console.Write($"{suly}");
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+
+            int[] libak = { 1, 5, 2, 3, 4 };
+            Kiir(libak,"libak sulyai");
+            Console.WriteLine("gomb");
+            Console.ReadKey();
+        }
+    }
+}
